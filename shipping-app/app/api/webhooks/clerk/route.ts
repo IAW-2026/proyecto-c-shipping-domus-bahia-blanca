f4cf7ea09bc7c16b9b44200bfc99a48933e4ff6a
Lautaro Nahid <lautaronahid54@gmail.com>
2026-05-17 17:05:22 -0300

    hotfix

--- shipping-app/app/api/webhooks/clerk/route.ts
+++ shipping-app/app/api/webhooks/clerk/route.ts
@@ -31,7 +31,7 @@ export async function POST(req: NextRequest) {
       )
     }
 
-    const headerPayload = headers()
+    const headerPayload = await headers()
 
     const svixId = headerPayload.get('svix-id')
     const svixTimestamp = headerPayload.get('svix-timestamp')
